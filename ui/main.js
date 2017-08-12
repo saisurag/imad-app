479b7ee5684df65b9c01c4df2773afd1b28081e1
saisurag <30898096+saisurag@users.noreply.github.com>
2017-08-12 13:40:34 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 // Move the image
+
 var img = document.getElementById('madi');
 img.oneclick = function () {
     img.style.marginLeft = '1000px';
