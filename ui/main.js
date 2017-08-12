916425ef66ef6bc6fe9d7c966db3a46cf7804122
saisurag <30898096+saisurag@users.noreply.github.com>
2017-08-12 13:47:06 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,9 +11,9 @@ element.innerHTML = 'New value';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft += 10;
+    marginLeft += 5;
     img.style.marginLeft = marginLeft + 'px'
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 };
