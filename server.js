505f75795534c121f096e5c8e05b065de338b645
saisurag <30898096+saisurag@users.noreply.github.com>
2017-08-12 19:30:43 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/submit-name/:name', function(req, res) {
     
     names.push(name);
     // JSON: Javascript Object Notation
-    res,send(JSON.stringify(names));
+    res.send(JSON.stringify(names));
 });
 
 
