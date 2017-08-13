4e25476588b5e7f5219fab6a850fbfb949c87fd5
saisurag <30898096+saisurag@users.noreply.github.com>
2017-08-13 10:13:00 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,20 +10,16 @@ button.onclick = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
           // taking some action
           if (request.status === 200) {
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+
           }
       }
     // Not done yet
   };
   
-  
   // Make the request
     request.open('GET', 'http://lsaisurag.imad.hasura-app.io/counter', true);
     request.send(null);
 };
-
 
 // Submit name
 
@@ -33,12 +29,31 @@ var submit = document.getElementById('submit.btn');
 submit.onclick = function () {
     // Make a request to the serer and send the name
     
-    // Capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i< names.length; i++) {
-        list += '<li>' + names[1] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+  // Create a request object
+  var request = new XMLHttpRequest();
+  
+  // Capture the response and storing it in a variable
+  request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // taking some action
+          if (request.status === 200) {
+              // Capture a list of names and render it as a list
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for (var i=0; i< names.length; i++) {
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ui = document.getElementById('namelist');
+              ul.innerHTML = list;
+              }
+          }
+      }
+    // Not done yet
+  };
+  
+  
+  // Make the request
+    request.open('GET', 'http://lsaisurag.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
 };
