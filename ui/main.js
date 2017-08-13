6a069de893be0fe651faa70b67634fbbdadd1482
saisurag <30898096+saisurag@users.noreply.github.com>
2017-08-13 10:20:40 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,6 @@ button.onclick = function () {
 
 // Submit name
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit.btn');
 submit.onclick = function () {
     
@@ -52,8 +50,9 @@ submit.onclick = function () {
     // Not done yet
   };
   
-  
   // Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://lsaisurag.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
