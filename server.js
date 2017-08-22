5e06d8093b9fb4b879bd0c4a6f489284b12cf112
saisurag <30898096+saisurag@users.noreply.github.com>
2017-08-22 08:47:54 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/submit-name', function(req, res) { // URL: /submit-name?name=xxxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('/article/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
   
   pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
       if (err) {
