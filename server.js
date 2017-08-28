3098c3d8aa46bf3de1dac5725560e88de4337f53
saisurag <30898096+saisurag@users.noreply.github.com>
2017-08-28 15:30:21 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'lsaisurag',
@@ -16,7 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.user(bodyParser.json());
-
+app.use(session({
+    sercet: 'someRandomSecretValue'
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 function createTemplate (data) {
     var title = data.title;
     var heading = data.heading;
