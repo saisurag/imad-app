b93336342eeea8f4d7ca68f8cd0af7e4940c6077
saisurag <30898096+saisurag@users.noreply.github.com>
2017-08-28 14:34:45 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,10 +57,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash (input) {
+function hash (input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
-};
+}
 
 
 app.get('/hash/:input', function(req, res) {
