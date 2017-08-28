fa3dc8b7c5a639c1c502dabaa5783fae0acb8825
saisurag <30898096+saisurag@users.noreply.github.com>
2017-08-28 15:16:12 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,4 @@
-var button = document.getElementById('counter');
 
-button.onclick = function () {
-    
-  // Create a request object
-  var request = new XMLHttpRequest();
-  
-  // Capture the response and storing it in a variable
-  request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          // Take some action
-          if (request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-          }
-          
-      }
-    // Not done yet
-  };
-  
-  // Make the request
-    request.open('GET', 'http://lsaisurag.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
 
 // Submit name
 var submit = document.getElementById('submit.btn');
